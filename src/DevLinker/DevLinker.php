47c80f6d80681d43a8b54ac0cc9522b53eda9d8c
Robert Goldsmith <r.s.goldsmith@far-blue.co.uk>
2015-07-24 17:22:59 +0100

    debugging to track down an issue with symlink creation

--- src/DevLinker/DevLinker.php
+++ src/DevLinker/DevLinker.php
@@ -57,6 +57,7 @@ class DevLinker extends LibraryInstaller
 
 		$this->initializeVendorSubdir($package);
 
+		echo "Attempting to symlink $localPath from {$this->getInstallPath($package)}\n";
 		if (true !== @symlink($localPath, $this->getInstallPath($package))) {
 			throw new \Exception('Symlinking of "' . $localPath . '" failed');
 		}
